66b1b03f847ecf6ef07ebb411439eb5419ff1b6f
xxc <89404@qq.com>
2024-09-27 17:23:20 +0800

    003_完成安装组件逻辑与测试

---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+/* 
+ Button.vue
+ Button.test.tsx
+ types.ts
+ style.css
+*/
+defineOptions({
+  name: "ErButton",
+});
+</script>
+
+<template>
+  <button style="background-color: blue; color: red">this is a button</button>
+</template>
